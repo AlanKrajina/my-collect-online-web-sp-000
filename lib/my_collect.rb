a9c5f3c48c30f092e3b3adc5346ffc54dff0a4c1
Alan Krajina <alankrajina88@gmail.com>
2019-05-11 13:15:36 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -17,6 +17,18 @@ my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]) do |name|
   name.split(" ").first
 end
 
+def my_collect(collection)
+  i=0
+  
+  while i < collection.length
+    
+  yield(collection[i])
+  
+  i += 1
+  end
+  collection
+end
+
 my_collect(["ruby", "javascript", "python", "objective-c"]) do |name|
   name.upcase
 end
